aa87acce5a47a8b2d28914a7f0924aa1623b7ccc
Zhiting Zhu <zhitingz@cs.utexas.edu>
2024-06-20 19:06:23 +0000

    remove warnings
    Signed-off-by: Zhiting Zhu <zhitingz@cs.utexas.edu>

--- new_delete.cpp
+++ new_delete.cpp
@@ -3,37 +3,22 @@
 using namespace std;
 
 extern "C" {
-    void* malloc(size_t);
-    void free(void*);
+void *malloc(size_t);
+void free(void *);
 }
 
-void* operator new(size_t size) throw (std::bad_alloc)
-{
-    return malloc(size);
+void *operator new(size_t size) { return malloc(size); }
+
+void *operator new(size_t size, const std::nothrow_t &) throw() {
+  return malloc(size);
 }
 
-void * operator new(size_t size, const std::nothrow_t&) throw()
-{
-    return malloc(size);
+void operator delete(void *ptr) { free(ptr); }
+
+void *operator new[](size_t size) { return malloc(size); }
+
+void *operator new[](size_t size, const std::nothrow_t &) {
+  return malloc(size);
 }
 
-void operator delete(void *ptr)
-{
-    free(ptr);
-}
-
-void* operator new[](size_t size) throw (std::bad_alloc)
-{
-    return malloc(size);
-}
-
-void * operator new[](size_t size, const std::nothrow_t&) throw()
-{
-    return malloc(size);
-}
-
-void operator delete[](void *ptr)
-{
-    free(ptr);
-}
-
+void operator delete[](void *ptr) { free(ptr); }
